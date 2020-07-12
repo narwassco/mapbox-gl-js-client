c4f303bbbf6d8f89093e0182d4337818093dcc97
Jin IGARASHI <wushilan0623@gmail.com>
2020-07-12 13:53:06 +0900

    disabled traffic button and added compass control instead default one.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import MapboxTraffic from '@mapbox/mapbox-gl-traffic';
 import RulerControl from 'mapbox-gl-controls/lib/ruler';
+import CompassControl from 'mapbox-gl-controls/lib/compass';
 import { MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher";
 import PitchToggle from './pitchtogglecontrol/pitchtogglecontrol';
 import AreaSwitcherControl from './AreaSwitcherControl/AreaSwitcherControl';
@@ -27,12 +28,13 @@ $(function(){
         attributionControl: false,
     });
 
-    this.map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    this.map.addControl(new mapboxgl.NavigationControl({showCompass:false}), 'top-right');
+    this.map.addControl(new CompassControl(), 'top-right');
     this.map.addControl(new mapboxgl.GeolocateControl({positionOptions: {enableHighAccuracy: true},trackUserLocation: true}), 'top-right');
     this.map.addControl(new PitchToggle({minpitchzoom: 19})); 
     MapboxStyleSwitcherControl.DEFAULT_STYLE = config.styles[0].title;
     this.map.addControl(new MapboxStyleSwitcherControl(config.styles), 'top-right');
-    this.map.addControl(new MapboxTraffic({showTraffic:false}));
+    // this.map.addControl(new MapboxTraffic({showTraffic:false}));
     this.map.addControl(new AreaSwitcherControl(), 'top-right');
     this.map.addControl(new RulerControl(), 'top-right');
     this.map.addControl(new mapboxgl.ScaleControl({maxWidth: 80, unit: 'metric'}), 'bottom-left');
